f16c0ec88c8749001e80f435533705ae67a533b9
Verboofed <caseypetrosky16@gmail.com>
2022-05-18 20:44:13 -0500

    initial

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body class="bg-primary-200 text-white">
+  <div class="paralax"><!-- Content Wrapper -->
+    <nav>
+     <main class="flex w-full bg-primary-100 text-xl space-x-6">
+       <h1 class="pl-7 justify-start ">Album Reviews</h1>
+       <a href = "index.html">Home</a>
+       <a>New Album</a>
+       <a>Edit Review</a>
+       <a>Sign In</a>
+       <a>Sign Out</a>
+     </main>
+    </nav><!-- End Nav -->
+
+    <main class="albumStorage">
+     <div class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+         <!-- album cards -->
+         <div class="card ">
+           <img src="img/Igor.png">
+           <div class="m-4">
+             <span class="text-primary-400">Igor</span>
+             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
+           </div>
+         </div>
+
+         <div class="card ">
+           <img src="img/MelodicBlue.jpeg">
+           <div class="m-4">
+             <span class="text-primary-400">Melodic Blue</span>
+             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
+           </div>
+         </div>
+
+         <div class="card ">
+           <img src="img/cmiygl.jpg">
+           <div class="m-4">
+             <span class="text-primary-400">Call me if you get lost</span>
+             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
+           </div>
+         </div>
+
+         <div class="card">
+           <img src="img/cmiygl.jpg">
+           <div class="m-4">
+             <span class="text-primary-400">Call me if you get lost</span>
+             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
+           </div>
+         </div>
+
+         <div class="card ">
+           <img src="img/cmiygl.jpg">
+           <div class="m-4">
+             <span class="text-primary-400">Call me if you get lost</span>
+             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
+           </div>
+         </div>
+     </div>
+   </main>
+
+  </div>
+</body>
