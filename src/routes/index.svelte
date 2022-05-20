5c136f8d58aa3aa65181245dd2eb327b045de5aa
Verboofed <caseypetrosky16@gmail.com>
2022-05-20 11:54:14 -0500

    commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,24 @@
-<body class="bg-primary-200 text-white">
-  <div class="paralax"><!-- Content Wrapper -->
-    <nav>
-     <main class="flex w-full bg-primary-100 text-xl space-x-6">
-       <h1 class="pl-7 justify-start ">Album Reviews</h1>
-       <a href = "index.html">Home</a>
-       <a>New Album</a>
-       <a>Edit Review</a>
-       <a>Sign In</a>
-       <a>Sign Out</a>
-     </main>
-    </nav><!-- End Nav -->
+<script>
+import MusicCard from "../components/MusicCard.svelte";
 
-    <main class="albumStorage">
-     <div class="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-         <!-- album cards -->
-         <div class="card ">
-           <img src="img/Igor.png">
-           <div class="m-4">
-             <span class="text-primary-400">Igor</span>
-             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
-           </div>
-         </div>
+  
+</script>
 
-         <div class="card ">
-           <img src="img/MelodicBlue.jpeg">
-           <div class="m-4">
-             <span class="text-primary-400">Melodic Blue</span>
-             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
-           </div>
-         </div>
+  <main class="albumStorage">
+   <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+       <!-- album cards -->
 
-         <div class="card ">
-           <img src="img/cmiygl.jpg">
-           <div class="m-4">
-             <span class="text-primary-400">Call me if you get lost</span>
-             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
-           </div>
-         </div>
+      <MusicCard albumTitle = {"Igor"} artist = {"Tyler, The Creator"} rating = {"9.7/10.0"}/>
 
-         <div class="card">
-           <img src="img/cmiygl.jpg">
-           <div class="m-4">
-             <span class="text-primary-400">Call me if you get lost</span>
-             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
-           </div>
-         </div>
+      <MusicCard albumTitle = {"The Melodic Blue"} artist = {"Baby Keem"} rating = {"8.7/10.0"}/>
 
-         <div class="card ">
-           <img src="img/cmiygl.jpg">
-           <div class="m-4">
-             <span class="text-primary-400">Call me if you get lost</span>
-             <span class="block text-primary-400 text-sm">9.2/10 - Good</span>
-           </div>
-         </div>
-     </div>
-   </main>
+      <MusicCard albumTitle = {"Call Me If You Get Lost"} artist = {"Tyler, The Creator"} rating = {"9.7/10.0"}/>
 
-  </div>
-</body>
+      <MusicCard albumTitle = {"Flower Boy"} artist = {"Tyler, The Creator"} rating = {"9.7/10.0"}/>
+
+      <MusicCard albumTitle = {"Donda"} artist = {"Kanye West"} rating = {"6.7/10.0"}/>
+
+      <MusicCard albumTitle = {"Blonde"} artist = {"Frank Ocean"} rating = {"10.0/10.0"}/>
+
+   </div>
+ </main>
